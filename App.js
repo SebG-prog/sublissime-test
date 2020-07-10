5b795797ed3eab36800d890842c1ad3f3848b664
SebG-prog <sebastien-gruet@orange.fr>
2020-07-10 11:14:26 +0200

    Implemented Tab navigation between components Pokedex and Sac

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Pokedex from './components/Pokedex';
 import Sac from './components/Sac';
 
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>This is a test</Text>
-      <Pokedex />
-      <Sac />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Pokedex" component={Pokedex} />
+        <Tab.Screen name="Sac" component={Sac} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
