c0c6497be4b28a180c60a9fb4ff6a384cd2d380a
SebG-prog <sebastien-gruet@orange.fr>
2020-07-10 13:30:53 +0200

    Swicth to MaterialTabNavigation to enable swipe ability

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import Pokedex from './components/Pokedex';
 import Sac from './components/Sac';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Pokedex"
+        tabBarPosition="bottom"
         tabBarOptions={{
+          showIcon: true,
           activeTintColor: '#EE4542',
         }}>
         <Tab.Screen
